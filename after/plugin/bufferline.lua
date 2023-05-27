46c37e2d4d88b29e462af3e97de10017f3f679f5
Andrew <mcdude22@gmail.com>
2023-05-27 13:39:32 -0700

    keymap fixes

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,7 +1,11 @@
 vim.opt.termguicolors = true
 require("bufferline").setup{}
 
-vim.keymap.set("n", "<C-h>", ":bprevious<CR>")
-vim.keymap.set("n", "<C-l>", ":bnext<CR>")
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>")
+vim.keymap.set("n", "<S-l>", ":bnext<CR>")
 vim.keymap.set("n", "<leader>c", ":bdelete<CR>")
 
+vim.keymap.set("n", "<C-h>", "<C-W>h")
+vim.keymap.set("n", "<C-j>", "<C-W>j")
+vim.keymap.set("n", "<C-k>", "<C-W>k")
+vim.keymap.set("n", "<C-l>", "<C-W>l")
--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,3 +1,5 @@
 
 vim.keymap.set("n", "<A-1>", ":ToggleTerm size=10 direction=horizontal<CR>")
 vim.keymap.set("i", "<A-1>", "<esc>:ToggleTerm size=10 direction=horizontal<CR>")
+vim.keymap.set("t", "<A-1>", "<esc>:ToggleTerm size=10 direction=horizontal<CR>")
+
