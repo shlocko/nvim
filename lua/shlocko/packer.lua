6e1291e3e47fc9529e7a00ef0b6b18a62a7beea6
Andrew <mcdude22@gmail.com>
2023-05-27 14:14:28 -0700

    Added which-key

--- lua/shlocko/packer.lua
+++ lua/shlocko/packer.lua
@@ -98,4 +98,16 @@ return require('packer').startup(function(use)
             require 'alpha'.setup(require 'alpha.themes.startify'.config)
         end
     }
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
 end)
