abca214babc59806aad3df9342a6ae7e48291c95
Andrew <mcdude22@gmail.com>
2023-06-12 11:36:46 -0700

    Fixed minor bug in keymaps

--- lua/shlocko/remap.lua
+++ lua/shlocko/remap.lua
@@ -1,6 +1,3 @@
-vim.keymap.set("n", "<leader>td", function() vim.cmd.colorscheme("nordic") end)
-vim.keymap.set("n", "<leader>tl", function() vim.cmd.colorscheme("catppuccin-latte") end)
-
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
@@ -56,3 +53,8 @@ end)
 
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("i", "<A-;>", "<esc>A;")
+
+
+vim.keymap.set("n", "<leader>td", function() vim.cmd.colorscheme("nordic") end)
+vim.keymap.set("n", "<leader>tl", function() vim.cmd.colorscheme("catppuccin-latte") end)
+
